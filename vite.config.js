5ed20a8d4792f28c35e4fa5e945ef9f25a5dba07
isulaillepe <isulaillepe2024@gmail.com>
2026-01-04 18:33:14 +0530

    creating a web

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This ensures the server runs on port 3000, which is standard
+  // This sets the base URL for the app to match your repo name
+  base: '/House-move/',
   server: {
     port: 3000,
   }
