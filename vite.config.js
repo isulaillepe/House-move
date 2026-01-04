4edad88b2a94b9639ae23a34446445f2406660f8
isulaillepe <isulaillepe2024@gmail.com>
2026-01-04 18:00:08 +0530

    THird Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This ensures the server runs on port 3000, which is standard
+  server: {
+    port: 3000,
+  }
 })
